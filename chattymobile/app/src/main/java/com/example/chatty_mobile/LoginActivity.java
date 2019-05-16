359f52c6a9a2bff831d3ccfb69243bda54122b6d
Oliver Sylvest Hansen <oliv9858@easv365.dk>
2019-05-16 12:30:35 +0200

    structure changes

--- chattymobile/app/src/main/java/com/example/chatty_mobile/LoginActivity.java
+++ chattymobile/app/src/main/java/com/example/chatty_mobile/LoginActivity.java
@@ -1,13 +1,27 @@
 package com.example.chatty_mobile;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+    private Button createBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        final Intent create = new Intent(this, CreateActivity.class);
+        createBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(create);
+            }
+        });
     }
 }

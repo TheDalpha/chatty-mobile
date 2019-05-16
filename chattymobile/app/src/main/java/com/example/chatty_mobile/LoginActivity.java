e9d8cef522d3b5d308c5cfe8da658ce5be218c80
MissTizz <tizz3n@hotmail.com>
2019-05-16 13:34:32 +0200

    Firebase Connect

--- chattymobile/app/src/main/java/com/example/chatty_mobile/LoginActivity.java
+++ chattymobile/app/src/main/java/com/example/chatty_mobile/LoginActivity.java
@@ -17,6 +17,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
         final Intent create = new Intent(this, CreateActivity.class);
+
+        createBtn = findViewById(R.id.btnCreate);
         createBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

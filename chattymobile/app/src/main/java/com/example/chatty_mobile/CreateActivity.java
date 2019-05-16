62fa96e56b380022f0f614aa3cd9c6bf00032b3f
Oliver Sylvest Hansen <oliv9858@easv365.dk>
2019-05-16 13:52:29 +0200

    Create sketch

--- chattymobile/app/src/main/java/com/example/chatty_mobile/CreateActivity.java
+++ chattymobile/app/src/main/java/com/example/chatty_mobile/CreateActivity.java
@@ -2,12 +2,18 @@ package com.example.chatty_mobile;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class CreateActivity extends AppCompatActivity {
+
+    private Button createBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        getSupportActionBar().hide();
+
+        createBtn = findViewById(R.id.btnCreate);
     }
 }

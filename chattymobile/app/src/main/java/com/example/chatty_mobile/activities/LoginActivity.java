b7bbfa2ade57ec01cbab4cad96c81377d4ef5883
MissTizz <32457342+MissTizz@users.noreply.github.com>
2019-05-22 00:11:23 +0200

    Goin sumwhere!

--- chattymobile/app/src/main/java/com/example/chatty_mobile/activities/LoginActivity.java
+++ chattymobile/app/src/main/java/com/example/chatty_mobile/activities/LoginActivity.java
@@ -1,41 +1,124 @@
 package com.example.chatty_mobile.activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.chatty_mobile.R;
-import com.example.chatty_mobile.services.MessageService;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-  
-    private Button createBtn;
-    private Button loginBtn;
+
+    private static final String TAG = "LOGGED IN";
+
+    private Button btnCreate;
+    private Button btnLogin;
+    private TextView txtEmail;
+    private TextView txtPassword;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
-        final Intent create = new Intent(this, CreateActivity.class);
         final Intent chat = new Intent(this, ChatActivity.class);
-      
-        createBtn = findViewById(R.id.btnCreate);
-        createBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(create);
-            }
-        });
 
-        loginBtn = findViewById(R.id.btnLogin);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Buttons
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCreate = findViewById(R.id.btnCreate);
+
+        // Views
+        txtEmail = findViewById(R.id.fieldEmail);
+        txtPassword = findViewById(R.id.fieldPassword);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(chat);
             }
         });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "-----------------signInWithEmail:success-----------------------------");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "--------------------------Authentication failed------------------------------.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            Log.d(TAG, "-------------------signInWithEmail:failed---------------------------");
+                        }
+                        // [END_EXCLUDE]
+                    }
+
+                });
+        // [END sign_in_with_email]
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = txtEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            txtEmail.setError("Required.");
+            valid = false;
+        } else {
+            txtEmail.setError(null);
+        }
+
+        String password = txtPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            txtPassword.setError("Required.");
+            valid = false;
+        } else {
+            txtPassword.setError(null);
+        }
+
+        return valid;
+    }
+
+
 }
+
